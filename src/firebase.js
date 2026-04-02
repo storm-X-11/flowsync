77fbabc0ff0eaa85d77de1e1d32bd48893e9b076
storm-X-11 <anshshrivastav621983@gmail.com>
2026-04-02 16:28:55 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0rTdOFM1-8dFrPsCpmqDe-zlLcXowjYU",
+  authDomain: "flowsync-f4d42.firebaseapp.com",
+  projectId: "flowsync-f4d42",
+  storageBucket: "flowsync-f4d42.firebasestorage.app",
+  messagingSenderId: "340749255218",
+  appId: "1:340749255218:web:0bc6b0a2bfd6506279322b"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
