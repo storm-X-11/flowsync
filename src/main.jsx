89724bdd28f5bbae53310c83b015b235b7a56c30
storm-X-11 <anshshrivastav621983@gmail.com>
2026-04-13 02:24:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
+// Register PWA service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').catch(() => {});
