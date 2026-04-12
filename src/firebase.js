8d1929afd41940c5fb83d60138039ac89f1d5366
storm-X-11 <anshshrivastav621983@gmail.com>
2026-04-13 02:21:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC0rTdOFM1-8dFrPsCpmqDe-zlLcXowjYU",
@@ -13,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
